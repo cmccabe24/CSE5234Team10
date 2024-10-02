1fde99ca0949372959f049b4e277edbedee42add
Connor McCabe <mccabe.256@osu.edu>
2024-10-02 18:58:18 -0400

    starting working on viewOrder

--- noodledoodle/src/components/viewOrder.js
+++ noodledoodle/src/components/viewOrder.js
@@ -1,1 +1,15 @@
-//test
+import React from 'react';
+
+const ViewOrder = () => {
+  const title = "View Order"; // You can define the title variable like this
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>Your order details will be displayed here.</p>
+      {/* You can add more JSX to display actual order information */}
+    </div>
+  );
+}
+
+export default ViewOrder;
