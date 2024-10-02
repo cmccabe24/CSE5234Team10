c4e019cd4ee7e0fcd4f5d4785917b6ae748de468
Nishtha Jindal <jindalnishtha@gmail.com>
2024-10-02 18:57:23 -0400

    updated shipping and payment entry pages

--- noodledoodle/src/components/paymentEntry.js
+++ noodledoodle/src/components/paymentEntry.js
@@ -1,1 +1,60 @@
 //test
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const paymentEntry = () => {
+	let location = useLocation();
+	const [order, setOrder ] = location.state;
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+	    navigate('/purchase/paymentEntry', {order: order, setOrder: setOrder});
+    } 
+	console.log('order: ', order);
+
+return ( 
+    <div>
+	<form onSubmit ={handleSubmit}>
+		<label>Credit Card Number</label>
+		    <input
+				type='text'
+				required
+				onChange={(e) =>
+					{order.credit_card_numer = e.target.value;}
+				}
+			/>
+			<br/>
+        <label>Expiration Date</label>
+			<input
+				type='text'
+				required
+				onChange={(e) =>
+					{order.expir_date = e.target.value;}
+				}
+			/>
+			<br/>
+		<label>CVV Code</label>
+			<input
+				type='text'
+				required
+				onChange={(e) =>
+					{order.cvvCode = e.target.value;}
+				}
+			/>
+			<br/>
+		<label>Credit Holder Name</label>
+            <input
+				type='text'
+				required
+				onChange={(e) =>
+					{order.card_holder_name = e.target.value;}
+				}
+			/>
+			<br/>
+			<button className='button'>Shipping Information</button>
+			</form>
+		</div>
+    );		
+};
+
+export default paymentEntry;
--- noodledoodle/src/components/viewOrder.js
+++ noodledoodle/src/components/viewOrder.js
@@ -1,1 +1,26 @@
 //test
+import React from 'react';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const viewOrder = () => {
+
+    let location = useLocation();
+    const[order, setOrder] = location.state;
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        navigate('/purchase/viewConfirmation', {order:order, setOrder:setOrder});
+    }
+
+    return (
+        <div>
+            <h1>
+                Product 1 {location.state.order.buy}
+            </h1>
+        </div>
+
+
+
+    );
+    
+};
