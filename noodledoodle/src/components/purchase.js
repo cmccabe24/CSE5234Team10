1ae22621c2534a256290c91766c3c4fd3d5b5e87
Mohisha Patel <patel.4088@osu.edu>
2024-10-02 18:43:34 -0400

    changes to purchase

--- noodledoodle/src/components/purchase.js
+++ noodledoodle/src/components/purchase.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const purchase = () => {
+    let title = "purchase page";
 
-	let title = "purchase page";
-	
-	
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    );
+};
+
+export default purchase;
+
